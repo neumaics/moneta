ce990eeff2ea9e7f45e2adbfbfb97952ea034a32
Brian Holland <neumaics@gmail.com>
2017-09-20 23:39:28 -0600

    begin react

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 const gulp = require('gulp');
 const jasmine = require('gulp-jasmine');
 const nodemon = require('gulp-nodemon');
-
+const babel = require('gulp-babel');
+const clean = require('gulp-clean');
 gulp.task('default', ['serve']);
 
 gulp.task('serve', () => {
   const stream = nodemon({
     script: 'server.js',
-    ext: 'html js'
+    ext: 'html js',
+    tasks: ['scripts'],
+    ignore: ['public']
   });
 
   stream
@@ -23,4 +26,18 @@ gulp.task('serve', () => {
 gulp.task('test', () => {
   gulp.src('test/**/*-test.js')
     .pipe(jasmine({ errorOnFail: false }));
+});
+
+gulp.task('scripts', ['clean'], () => {
+  gulp.src('app/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('public'));
+
+  return gulp.src('./app/index.html')
+    .pipe(gulp.dest('./public/'));
+});
+
+gulp.task('clean', function () {
+  return gulp.src('public', { read: false })
+    .pipe(clean());
 });
