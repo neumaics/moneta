f05156160b1f7dbab4b08e043df04bc6581ec4ff
Brian Holland <neumaics@gmail.com>
2017-09-21 23:56:13 -0600

    showing results on the ui

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -1,3 +1,5 @@
+/* eslint-env browser */
+/* global React ReactDOM axios */
 window.react = React;
 window.reactDom = ReactDOM;
 window.axios = axios;
--- app/components/currency-pair.js
+++ app/components/currency-pair.js
@@ -15,7 +15,7 @@ export default class CurrencyPair extends React.Component {
   }
 
   componentDidMount() {
-    axios.get(`/ticker/BTC_LTC`)
+    axios.get(`/ticker/${this.props.pair}`)
       .then((response) => {
         const exchanges = response.data;
 
@@ -25,10 +25,24 @@ export default class CurrencyPair extends React.Component {
 
   render() {
     const { pair } = this.props;
+    const [ base, quote ] = pair.split('_');
+    const { exchanges } = this.state;
 
-    console.log(this.state);
+    const echangeElements = exchanges
+      .filter((ticker) => !ticker.hasOwnProperty('error'))
+      .map((ticker) => {
+        const rate = 1 / ticker.ask;
+        const style = { color: ticker.best ? 'green' : 'black' };
 
-    return <h5>{pair}</h5>;
+        return <h5 style={style} key={ticker.exchange}>{ticker.exchange}: 1 {base} / {rate} {quote}</h5>;
+      });
+
+    return (
+      <div>
+        <h4>{pair}</h4>
+        {echangeElements}
+      </div>
+    );
   }
 }
 
