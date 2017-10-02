897bbe6150359592e24f26ae27a19db4eed87d50
Brian Holland <neumaics@gmail.com>
2017-10-01 21:20:22 -0600

    get history working

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -1,5 +1,12 @@
 /* eslint-env browser */
-/* global React ReactDOM axios */
+/*
+  global 
+  React ReactDOM PropTypes
+  axios
+  Recharts
+*/
 window.react = React;
 window.reactDom = ReactDOM;
+window.propTypes = PropTypes;
 window.axios = axios;
+window.recharts = Recharts;
